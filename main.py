7c662a18eece289ce41a672e112cd02a526c5868
Jack Laxson <jackjrabbit@gmail.com>
2019-08-10 18:32:16 -0400

    base functionality, ran black on it

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import hashlib
 import hmac
 
-
 import requests
 import toml
-from bottle import post, run, request
+from bottle import post, run, request, debug
 
-def verify(api_key, token, timestamp, signature):
+
+def verify(api_key: bytes, token: bytes, timestamp: bytes, signature: bytes):
     "from mg docs"
-    hmac_digest = hmac.new(key=api_key,msg='{}{}'.format(timestamp, token),digestmod=hashlib.sha256).hexdigest()
-    return hmac.compare_digest(unicode(signature), unicode(hmac_digest))
+    hmac_digest = hmac.new(
+        key=api_key,
+        msg="{}{}".format(timestamp, token).encode(),
+        digestmod=hashlib.sha256,
+    ).hexdigest()
+    return hmac.compare_digest(signature, hmac_digest)
+
 
 @post("/mailgun/in")
 def incoming_mg():
     print(request.json)
+    if verify(
+        MG_API_KEY,
+        request.json["signature"]["token"],
+        request.json["signature"]["timestamp"],
+        request.json["signature"]["signature"],
+    ):
+        formatted_msg = f"{request.json['event-data']['event']} :thinking: \n{request.json['event-data']['recipient']} \n ```json\n{request.json['event-data']['delivery-status']}```"
+        shove2discord(formatted_msg)
+
 
 def shove2discord(msg):
-    ret = requests.post(DISCORD_URL, data={'content': "pee pee poo poo"})
+    ret = requests.post(DISCORD_URL, data={"content": msg})
     ret.raise_for_status()
+
 
 if __name__ == "__main__":
     conf = toml.load("config.toml")
-    MG_API_KEY  = conf["mg_signing_key"]
+    MG_API_KEY = conf["mg_signing_key"].encode()
     DISCORD_URL = conf["discord_webhook_url"]
-    shove2discord("testing")
-    run()
+    # shove2discord("testing")
+    debug(True)
+    run(reloader=True)
